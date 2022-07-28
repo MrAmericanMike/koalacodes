1e64e123b511b215e450df0f7b246e21e8479937
MrAmericanMike <mramericanmike@gmail.com>
2022-07-28 16:47:14 -0300

    Added support to pass user defined arrays of vowels and consonants

--- README.md
+++ README.md
@@ -48,6 +48,18 @@ Using import syntax:
 import KoalaCodes from "koalacodes";
 ```
 
+User defined arrays of vowels and consonants
+```js
+const KC = new KoalaCodes({
+	defaultLength: 7,
+	vowels: ["а", "е", "ё", "и", "о", "у", "ы", "э", "ю", "я"],
+	consonants: ["б", "в", "г", "д", "ж", "з", "к", "л", "м", "н", "п", "р", "с", "т", "ф", "х", "ц", "ч", "ш", "щ"]
+});
+
+console.log(KC.generateCode(4));  // хажя
+console.log(KC.generateCode());   // рулакыч
+```
+
 ***
 
 ### Notes
@@ -58,7 +70,6 @@ At this stage the library will always generate a code that alternates consonants
 
 ### **TODO** and **Future Features**
 
-* User defined arrays of vowels and consonants
 * User defined starting letter (vowel/consonant)
 * Hability to control UpperCase and LowerCase letters
 
--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,43 @@
-interface options {
+interface defaults {
+	vowels: Array<string>;
+	consonants: Array<string>;
 	defaultLength: number;
 }
 
+interface options {
+	vowels?: Array<string>;
+	consonants?: Array<string>;
+	defaultLength?: number;
+}
+
+const DEFAULTS: defaults = {
+	vowels: ["A", "E", "I", "O", "U"],
+	consonants: ["B", "C", "D", "F", "G", "H", "J", "K", "L", "M", "N", "P", "Q", "R", "S", "T", "V", "W", "X", "Y", "Z"],
+	defaultLength: 8
+}
+
 class KoalaCodes {
-	#VOWELS = ["A", "E", "I", "O", "U"];
-	#CONSONANTS = ["B", "C", "D", "F", "G", "H", "J", "K", "L", "M", "N", "P", "Q", "R", "S", "T", "V", "W", "X", "Y", "Z"];
+	/**
+	 * @type {Array<string>}
+	 */
+	#VOWELS;
+	/**
+	 * @type {Array<string>}
+	 */
+	#CONSONANTS;
+	/**
+	 * @type {number}
+	 */
 	#DEFAULT_LENGTH;
-	constructor(options: options = { defaultLength: 8 }) {
-		this.#DEFAULT_LENGTH = options.defaultLength;
+
+	/**
+	 * 
+	 * @param {options} options Different options for constructing the code generator
+	 */
+	constructor(options?: options) {
+		this.#VOWELS = options?.vowels ? options.vowels : DEFAULTS.vowels;
+		this.#CONSONANTS = options?.consonants ?? DEFAULTS.consonants;
+		this.#DEFAULT_LENGTH = options?.defaultLength ?? DEFAULTS.defaultLength;
 	}
 
 	generateCode(length: number = this.#DEFAULT_LENGTH): string {
