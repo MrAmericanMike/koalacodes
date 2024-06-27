be9aa60e5e0cbb1cbe51c8dbc3816393d0440eec
MrAmericanMike <mramericanmike@gmail.com>
2024-06-27 16:05:31 -0300

    New option to select what type of letter to start with

--- src/main.ts
+++ src/main.ts
@@ -2,25 +2,28 @@ interface Defaults {
 	vowels: Array<string>;
 	consonants: Array<string>;
 	defaultLength: number;
+	firstLetter: "consonant" | "vowel";
 }
 
 interface Options {
 	vowels?: Array<string>;
 	consonants?: Array<string>;
 	defaultLength?: number;
+	firstLetter?: "consonant" | "vowel";
 }
 
 const DEFAULTS: Defaults = {
 	vowels: ["A", "E", "I", "O", "U"],
 	consonants: ["B", "C", "D", "F", "G", "H", "J", "K", "L", "M", "N", "P", "Q", "R", "S", "T", "V", "W", "X", "Y", "Z"],
-	defaultLength: 8
+	defaultLength: 8,
+	firstLetter: "consonant"
 };
 
 class KoalaCodes {
 	#VOWELS: Array<string>;
 	#CONSONANTS: Array<string>;
 	#DEFAULT_LENGTH: number;
-
+	#FIRST_LETTER: "consonant" | "vowel";
 	/**
 	 *
 	 * @param {Options} options Different options for constructing the code generator
@@ -29,6 +32,7 @@ class KoalaCodes {
 		this.#VOWELS = options?.vowels ?? DEFAULTS.vowels;
 		this.#CONSONANTS = options?.consonants ?? DEFAULTS.consonants;
 		this.#DEFAULT_LENGTH = options?.defaultLength ?? DEFAULTS.defaultLength;
+		this.#FIRST_LETTER = options?.firstLetter ?? DEFAULTS.firstLetter;
 	}
 
 	generateCode(length: number = this.#DEFAULT_LENGTH): string {
@@ -36,9 +40,17 @@ class KoalaCodes {
 		let code = "";
 		for (let index = 0; index < LENGTH; index++) {
 			if (index % 2 == 0) {
-				code += this.#CONSONANTS[Math.floor(Math.random() * this.#CONSONANTS.length)];
+				if (this.#FIRST_LETTER == "consonant") {
+					code += this.#CONSONANTS[Math.floor(Math.random() * this.#CONSONANTS.length)];
+				} else {
+					code += this.#VOWELS[Math.floor(Math.random() * this.#VOWELS.length)];
+				}
 			} else {
-				code += this.#VOWELS[Math.floor(Math.random() * this.#VOWELS.length)];
+				if (this.#FIRST_LETTER == "consonant") {
+					code += this.#VOWELS[Math.floor(Math.random() * this.#VOWELS.length)];
+				} else {
+					code += this.#CONSONANTS[Math.floor(Math.random() * this.#CONSONANTS.length)];
+				}
 			}
 		}
 		return code;
